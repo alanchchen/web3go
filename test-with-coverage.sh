08a340af0d50e2046d629b6716a4e44998dd45d7
Alan Chen <alan@maicoin.com>
2016-06-12 14:22:31 +0800

    Add code coverage script

---
+++ test-with-coverage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+echo "" > coverage.txt
+
+for d in $(find . -maxdepth 10 -type d); do
+    if ls $d/*.go &> /dev/null; then
+        go test -v -coverprofile=profile.out -covermode=atomic $d
+        if [ -f profile.out ]; then
+            cat profile.out >> coverage.txt
+            rm profile.out
+        fi
+    fi
+done
